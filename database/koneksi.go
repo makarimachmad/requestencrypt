40b31574536abb3fc00eb5bbc328b1be8e1a40d6
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-11-03 07:55:22 +0700

    create connection function

--- database/koneksi.go
+++ database/koneksi.go
@@ -1,1 +1,17 @@
 package database
+
+import (
+	"requestencrypt/config"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	dsn := config.Config("DB_USER")+":"+config.Config("DB_PASS")+"@("+config.Config("DB_HOST")+":"+config.Config("DB_PORT")+")/"+config.Config("DB_NAME")
+	db,err := gorm.Open(mysql.Open(dsn))
+	if err != nil{
+		return nil, err
+	}
+	return db, nil
+}
