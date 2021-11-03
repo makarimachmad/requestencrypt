23d14ec77ee359f9c3870232b8bb7e1fa0c1cd4b
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-11-03 07:57:29 +0700

    struct user

--- user/structure.go
+++ user/structure.go
@@ -1,1 +1,14 @@
 package user
+
+type(
+	Pengunjung struct{
+		ID		int	`json:"id"`
+		Nama		string	`json:"nama"`
+		Umur		int	`json:"umur"`
+		Alamat		string	`json:"alamat"`
+		Pekerjaan	string	`json:"pekerjaan"`
+		TanggalLahir	string	`json:"tanggal_lahir"`
+		CreatedDate	string	`json:"created_date"`
+	}
+	Pengunjungs	[]Pengunjung
+)
