d83394f232f368099d6e0d69b26a88667b6e7b65
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-11-03 07:57:02 +0700

    model crud for user pengunjung

--- user/model.go
+++ user/model.go
@@ -1,1 +1,44 @@
 package user
+
+import (
+	"requestencrypt/database"
+	"strconv"
+)
+
+func (p *Pengunjung) PengunjungPost()error {
+	db, err := database.ConnectDB()
+	if err != nil{
+		return err
+	}
+	db.Create(&p)
+	return nil
+}
+
+func (p *Pengunjungs) PengunjungGet() error {
+	db, err := database.ConnectDB()
+	if err != nil{
+		return err
+	}
+	db.Find(&p)
+	return nil
+}
+
+func (p *Pengunjung) PengunjungUpdate(id string) error{
+	db, err := database.ConnectDB()
+	if err != nil{
+		return err
+	}
+	i,_ := strconv.Atoi(id)
+	db.Model(&Pengunjung{}).Where("id = ?", i).Updates(&p)
+	return nil
+}
+
+func (p *Pengunjung) PengunjungDelete(id string) error{
+	db, err := database.ConnectDB()
+	if err != nil{
+		return err
+	}
+	i,_ := strconv.Atoi(id)
+	db.Delete(&p, i)
+	return nil
+}
