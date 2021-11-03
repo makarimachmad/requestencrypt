91703b1f63602dd645dfb79b4ad0706fcda9bf3c
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-11-03 18:19:58 +0700

    add function for validate required

--- user/structure.go
+++ user/structure.go
@@ -1,5 +1,9 @@
 package user
 
+import(
+	v "github.com/go-ozzo/ozzo-validation/v4"
+)
+
 type(
 	Pengunjung struct{
 		ID		int	`json:"id"`
@@ -11,4 +15,22 @@ type(
 		CreatedDate	string	`json:"created_date"`
 	}
 	Pengunjungs	[]Pengunjung
+
+	DatabaseKeys struct{
+		ID	int	`json:"id"`
+		UserID	int	`json:"user_id"`
+		Username	string	`json:"username"`
+		Password	string	`json:"password"`
+		EncryptionKey	string	`json:"encryption_key"`
+	}
 )
+
+func (p Pengunjung) Validate() error{
+	return v.ValidateStruct(&p,
+		v.Field(&p.Nama, v.Required),
+		v.Field(&p.Umur, v.Required),
+		v.Field(&p.Alamat, v.Required),
+		v.Field(&p.Pekerjaan, v.Required),
+		v.Field(&p.TanggalLahir, v.Required),
+	)
+}
