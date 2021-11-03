912b0353e4cd89540d383a764176e015340e40db
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-11-03 07:54:49 +0700

    create config function

--- config/config.go
+++ config/config.go
@@ -1,1 +1,15 @@
 package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Config(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil{
+		return "Gagal memuat file env"
+	}
+	return os.Getenv(key)
+}
