22833fbb53ecc33c50e9e667dfa39dc8d9477970
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-11-04 14:46:39 +0700

    model for insert data to database and model login

---
+++ user/model.login.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"errors"
+	"requestencrypt/database"
+	"strings"
+)
+
+func (d *DatabaseKeys) DatabaseKeyPost() error {
+	db, err := database.ConnectDB()
+	if err != nil {
+		return err
+	}
+	db.Create(&d)
+	return nil
+}
+
+func Auth(username, password string) (*DatabaseKeys, error){
+	var d DatabaseKeys
+	db, err := database.ConnectDB()
+	if err != nil{
+		return nil, err
+	}
+	
+	err = db.Where(map[string]interface{}{"username":username}).Find(&d).Error
+	
+	if err != nil{
+		return nil, errors.New("internal error")
+	}
+	if username == ""{
+		return nil, errors.New("pengguna tidak terdaftar")
+	}
+	md5pass := Md5([]byte(password))
+	if !strings.EqualFold(d.Password, md5pass){
+		return nil, errors.New("password salah")
+	}
+	return &d, nil
+}
