39c14bc215765b23c11d2ec5c89b459588a0e159
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-11-05 11:14:08 +0700

    add initiate encryption

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import(
+import (
+	"log"
+	"requestencrypt/kriptografi"
 	"requestencrypt/router"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main(){
 	e := echo.New()
 	
+	err := kriptografi.EncryptionInit()
+	if err != nil{
+		log.Println("error")
+	}
+
 	router.SetupRoutes(e)
 	e.Logger.Fatal(e.Start(":1323"))
 }
