a38e06f0ec72d7af27617bdc1053a3ed248b4874
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-12-02 10:57:34 +0700

    add new structure

--- kriptografi/structure.go
+++ kriptografi/structure.go
@@ -9,6 +9,11 @@ var (
 	Salt                       string
 	EncSalt, PreSalt, PostSalt []byte
 	Block                      cipher.Block
+
+	LisenceStatus bool
+	ClientID      string
+	ProductKEY    string
+	SecretKEY     string
 )
 
 type (
@@ -18,6 +23,7 @@ type (
 		Req        Request
 		ReqBuffer  []byte
 		Res        Response
+		Pon	   PonseRes
 	}
 
 	Request struct {
@@ -30,10 +36,49 @@ type (
 		Data          string      `json:"data,omitempty"`
 		Errors        string      `json:"errors,omitempty"`
 		DecryptedData interface{} `json:"decrypted_data,omitempty"`
+		Error	      bool	  `json:"error"`
+	}
+
+	PonseRes struct {
+		Status        string      `json:"status"`
+		Message       string      `json:"message"`
+		Data          string      `json:"data,omitempty"`
+		Errors        string      `json:"errors,omitempty"`
+		DecryptedData interface{} `json:"decrypted_data,omitempty"`
+	}
+	ResPengunjung struct {
+		Status        string      `json:"status"`
+		Message       string      `json:"message"`
+		Data          interface{} `json:"data"`
+		Errors        string      `json:"errors,omitempty"`
+		DecryptedData interface{} `json:"decrypted_data,omitempty"`
+		Error	      bool	  `json:"error"`
 	}
 
 	Pengunjung struct{
 		Req	[]byte
+		Response
 		Crypt
+		Res 	ResPengunjung
+		Respon Bantu
+	}
+
+	Pengunjungs []Pengunjung
+
+	Bantu struct{
+		ID		int	`json:"id"`
+		Nama		string	`json:"nama"`
+		Umur		int	`json:"umur"`
+		Alamat		string	`json:"alamat"`
+		Pekerjaan	string	`json:"pekerjaan"`
+		TanggalLahir	string	`json:"tanggal_lahir"`
+		CreatedDate	string	`json:"created_date"`
+	}
+
+	Resdek struct{
+		Error	bool		`json:"error"`
+		Message	string		`json:"message"`
+		Data	interface{}	`json:"data"`
+		Erorrs	interface{}	`json:"errors"`
 	}
 )
