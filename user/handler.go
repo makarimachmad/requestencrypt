19b857efa8394298690fc8a50a7e9340bd2c465c
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-12-02 11:22:26 +0700

    add decrypt in post new pengunjung

--- user/handler.go
+++ user/handler.go
@@ -1,102 +1,123 @@
 package user
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"requestencrypt/kriptografi"
 	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
-func PostPengunjung(ctx echo.Context) error{
+func PostPengunjung(ctx echo.Context) error {
+	fmt.Println("ctx: ", ctx)
 	resp := new(Response)
 	resp.Error = true
 	resp.Message = "error post"
 
 	pengunjung := new(Pengunjung)
-	DK := new(DatabaseKeys)
+	pengunjungdekrip := new(kriptografi.Pengunjung)
 
-	err := ctx.Bind(pengunjung)
-	if err != nil{
+	err := ctx.Bind(pengunjungdekrip)
+	if err != nil {
 		return ctx.JSON(http.StatusNotFound, resp)
 	}
 
-	lahirParse,_ := time.Parse("2006-01-02", pengunjung.TanggalLahir)
-	
-	ty,_,_ := time.Now().Date()
-	by,_,_ := lahirParse.Date()
+	pengunjungdekrip.CipherText = pengunjungdekrip.Data
 
-	usia := ty-by
-	
+	//start dekrip
+	errors := pengunjungdekrip.Decrypt(true)
+	if errors != nil {
+		fmt.Println("gagal dekrip")
+	}
+	//end dekrip
+
+	json.Unmarshal(pengunjungdekrip.PlainText, &pengunjung)
+
+	lahirParse, _ := time.Parse("2006-01-02", pengunjung.TanggalLahir)
+
+	ty, _, _ := time.Now().Date()
+	by, _, _ := lahirParse.Date()
+
+	usia := ty - by
+
 	pengunjung.Umur = int(usia)
 	pengunjung.CreatedDate = time.Now().String()
 
 	err = pengunjung.PengunjungPost()
-	if err != nil{
-		return ctx.JSON(http.StatusNotFound, resp)
-	}
-
-	DK.UserID = pengunjung.ID
-	DK.Username = "pengunjung1"
-	DK.EncryptionKey = "123"
-	DK.Password = "321"
-	DK.GenPwd()
-
-	err = DK.DatabaseKeyPost()
-	if err != nil{
+	if err != nil {
 		return ctx.JSON(http.StatusNotFound, resp)
 	}
 
 	resp.Error = false
-	resp.Message = "success post"
+	resp.Message = "success post dekrip"
 	resp.Data = pengunjung
-	return ctx.JSON(http.StatusOK, resp)
+
+	fmt.Println("--------------- enkrip response ----------")
+	responResp := new(kriptografi.Crypt)
+	responResp.PlainText, _ = json.Marshal(resp)
+	err = responResp.Encrypt(true)
+	if err != nil{
+		return err
+	}
+	fmt.Println("responResp enkrip: ", responResp)
+
+	return ctx.JSON(http.StatusOK, responResp)
 }
 
-func GetPengunjung(ctx echo.Context) error{
+func Coba(ctx echo.Context) error {
+	log.Println("hai")
+	return nil
+}
+
+func GetPengunjung(ctx echo.Context) error {
 	resp := new(Response)
 	resp.Error = true
 	resp.Message = "error get"
 
 	pengunjungs := new(Pengunjungs)
 	err := ctx.Bind(pengunjungs)
-	if err != nil{
+	if err != nil {
 		return ctx.JSON(http.StatusNotFound, resp)
 	}
 
 	err = pengunjungs.PengunjungGet()
-	if err != nil{
+	if err != nil {
 		return ctx.JSON(http.StatusNotFound, resp)
 	}
+
 	resp.Error = false
 	resp.Message = "success get"
 	resp.Data = pengunjungs
 	return ctx.JSON(http.StatusOK, resp)
 }
 
-func UpdatePengunjung(ctx echo.Context) error{
+func UpdatePengunjung(ctx echo.Context) error {
 	resp := new(Response)
 	resp.Error = true
 	resp.Message = "error update"
 
 	pengunjung := new(Pengunjung)
 	err := ctx.Bind(pengunjung)
-	if err != nil{
+	if err != nil {
 		return ctx.JSON(http.StatusNotFound, resp)
 	}
 
 	id := ctx.Param("idx")
 
-	lahirParse,_ := time.Parse("2006-01-02", pengunjung.TanggalLahir)
-	
-	ty,_,_ := time.Now().Date()
-	by,_,_ := lahirParse.Date()
+	lahirParse, _ := time.Parse("2006-01-02", pengunjung.TanggalLahir)
 
-	usia := ty-by
-	
+	ty, _, _ := time.Now().Date()
+	by, _, _ := lahirParse.Date()
+
+	usia := ty - by
+
 	pengunjung.Umur = int(usia)
 
 	err = pengunjung.PengunjungUpdate(id)
-	if err != nil{
+	if err != nil {
 		return ctx.JSON(http.StatusNotFound, resp)
 	}
 	resp.Error = false
@@ -105,7 +126,7 @@ func UpdatePengunjung(ctx echo.Context) error{
 	return ctx.JSON(http.StatusOK, resp)
 }
 
-func DeletePengunjung(ctx echo.Context) error{
+func DeletePengunjung(ctx echo.Context) error {
 	resp := new(Response)
 	resp.Error = true
 	resp.Message = "error delete"
@@ -113,7 +134,7 @@ func DeletePengunjung(ctx echo.Context) error{
 	id := ctx.Param("idx")
 	pengunjung := new(Pengunjung)
 	err := pengunjung.PengunjungDelete(id)
-	if err != nil{
+	if err != nil {
 		return ctx.JSON(http.StatusNotFound, resp)
 	}
 
